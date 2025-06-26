1e430fbc4e49ef17d67189c89d27fdc501010e22
DaveSinsioco <sinciocodave@gmail.com>
2025-06-27 04:06:25 +0800

    initialize enemy and adds alive checker

--- game_development_of_a_turn_based_game.py
+++ game_development_of_a_turn_based_game.py
@@ -9,3 +9,18 @@ class Player:
         self.attack_power = attack_power
         self.critical_chance = critical_chance
         self.critical_multiplier = critical_multiplier
+
+    def is_alive(self):
+        return self.health > 0    
+
+# initialize enemy data
+class Enemy:
+    def __init__(self, health=140, attack_power=10, critical_chance=0.8, critical_multiplier=2):
+        self.health = health
+        self.attack_power = attack_power
+        self.critical_chance = critical_chance
+        self.critical_multiplier = critical_multiplier
+        
+    def is_alive(self):
+        return self.health > 0    
+            
