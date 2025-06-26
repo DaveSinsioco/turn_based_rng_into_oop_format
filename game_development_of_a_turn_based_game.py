1a066f8758f0208c6ad52fc77bbaaf4ee5cffcf2
DaveSinsioco <sinciocodave@gmail.com>
2025-06-27 04:04:44 +0800

    initialize player data

--- game_development_of_a_turn_based_game.py
+++ game_development_of_a_turn_based_game.py
@@ -1,6 +1,11 @@
 import math
 import random
 
-class turn_based_game:
-    def __init__(self):
-        pass
+# initialize player data
+class Player:
+    def __init__(self, health=100, heal_amount=25, attack_power=12, critical_chance=0.10, critical_multiplier=2):
+        self.health = health
+        self.heal_amount = heal_amount
+        self.attack_power = attack_power
+        self.critical_chance = critical_chance
+        self.critical_multiplier = critical_multiplier
